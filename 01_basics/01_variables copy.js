3984b5811550b259b952addd88a0709e10fa9289
Titoo32145 <94115416+Titoo32145@users.noreply.github.com>
2025-06-26 21:07:55 +0530

    variables_class

---
+++ 01_basics/01_variables copy.js
@@ -0,0 +1,21 @@
+const accountId = 4584845;
+let accountEmail = "omprk24@gmail.com";
+var accountPassword = "98745693";
+accountCity = "Delhi";
+let accountState;
+
+//accountId = 2; // Not Allowed
+
+accountEmail = "om@gmail.com";
+accountPassword = "123456789";
+accountCity = "Mumbai";
+
+console.log(accountId)
+
+/*
+    Perfer not to use var
+    because or issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
